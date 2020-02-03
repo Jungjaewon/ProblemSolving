e91b160fb50c51b1f252486c8605a11f8189d6c1
Jungjaewon <Woodcook486@naver.com>
2020-02-03 21:05:34 +0900

    Add files via upload

---
+++ HackerRank_twoStrings/HackerRank_twoStrings/HackerRank_twoStrings.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+// Complete the twoStrings function below.
+string twoStrings(string s1, string s2) {
+	set<char> char_set;
+	for (char a : s1)
+		char_set.insert(a);
+
+	for (char a : s2) {
+
+		if (char_set.find(a) != char_set.end()) {
+			return "YES";
+		}
+
+	}
+	return "NO";
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
